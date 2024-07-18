ee968c4f315ccb22aaf1aff99d81d9b08f627e1f
garyatwalbirkbeck <56833408+garyatwalbirkbeck@users.noreply.github.com>
2024-07-18 10:38:06 +0000

    Tip calculator

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+print("Welcome to the tip calculator.")
+
+print(f"What was the total bill? ${input('What was the total bill? $')}")
+
+print(f"What percentage tip would you like to give? {input('What percentage tip would you like to give
 #If the bill was $150.00, split between 5 people, with 12% tip. 
 
 #Each person should pay (150.00 / 5) * 1.12 = 33.6
